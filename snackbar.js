0d847f9a73315db19115cf26d0ec4d7d44d6f703
Michael Cheng <michael@michaelcheng.us>
2015-06-19 16:41:25 +0800

    Initial commit

---
+++ snackbar.js
@@ -0,0 +1,25 @@
+/***********************************************
+
+  "snackbar.js"
+
+  Created by Michael Cheng on 06/19/2015 16:37
+            http://michaelcheng.us/
+            michael@michaelcheng.us
+            --All Rights Reserved--
+
+***********************************************/
+
+"use strict";
+
+var iqwerty = iqwerty || {};
+
+iqwerty.snackbar = (function() {
+	function Snackbar() {
+
+	};
+
+	
+	return {
+		Snackbar: Snackbar
+	};
+})();
